6abdf2d3b75d0f8d469aea2256b46d437936f91a
17688872576 <1012166512@qq.com>
2020-12-14 17:34:11 +0800

    ''

--- common/src/main/java/com/lzb/tester/common/DeveloperTest.java
+++ common/src/main/java/com/lzb/tester/common/DeveloperTest.java
@@ -3,6 +3,12 @@ package com.lzb.tester.common;
 public class DeveloperTest {
 
     public static void main(String[] args) {
+        try {
+            Thread.sleep(1300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread.yield();
         System.out.println();
     }
 }
