b4943f01d016fb2e812d6b20fe4c49bdf74658b8
17688872576 <1012166512@qq.com>
2020-12-15 15:04:56 +0800

    ''

--- common/src/main/java/com/lzb/tester/common/DeveloperTest.java
+++ common/src/main/java/com/lzb/tester/common/DeveloperTest.java
@@ -1,14 +1,9 @@
 package com.lzb.tester.common;
 
+
 public class DeveloperTest {
 
     public static void main(String[] args) {
-        System.out.println("hahaha你在哪?");
-        try {
-            Thread.sleep(1300);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         Thread.yield();
     }
 }
